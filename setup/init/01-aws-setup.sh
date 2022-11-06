695194202039403c8cc8b38baefe5bbf9254afb2
Mircea Adam <mirciulicah@gmail.com>
2022-11-06 13:50:50 +0200

    found bug with credentials

--- setup/init/01-aws-setup.sh
+++ setup/init/01-aws-setup.sh
@@ -6,7 +6,7 @@ echo $WORKDIR
 SETUP_PATH="/workspaces/$WORKDIR/.devcontainer"
 mkdir -p /root/.aws/
 ls $SETUP_PATH/.secrets/.aws/
-cp $SETUP_PATH/.secrets/.aws/* /root/.aws/
+cp $SETUP_PATH/.secrets/.aws/* /root/.aws/ #when container is rebuild this gets deleted
 echo -e "listing HOME_CONTAINER"
 ls ~
 echo -e "completed \xE2\x9C\x94"
@@ -14,5 +14,3 @@ echo -e "completed \xE2\x9C\x94"
 #Set Token at container init
 chmod +x $SETUP_PATH/awstools/aws-get-token.sh
 bash $SETUP_PATH/awstools/aws-get-token.sh
-
-rm -rf $SETUP_PATH/.secrets
