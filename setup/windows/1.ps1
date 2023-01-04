f97023d486843722335bcf2e30cd9b112ccddff4
Mircea Adam <mircea.adam@azets.com>
2023-01-04 14:58:38 +0000

    update 2

--- setup/windows/1.ps1
+++ setup/windows/1.ps1
@@ -2,3 +2,10 @@ dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /nores
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 #RESTART REQUIRED
 shutdown /r /t 0
+wsl --set-default-version 2
+wsl –update
+wsl –install -d ubuntu
+mkdir C:\temp\devops
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2204 -OutFile C:\temp\devops\Ubuntu.appx -UseBasicParsing #!takes too long
+wget https://aka.ms/wslubuntu2204
+Add-AppxPackage C:\temp\devopsUbuntu.appx
