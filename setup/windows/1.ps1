f1bb568f9f006d0f3416a54fe3e5b0abfef7a0f3
Mircea Adam <mircea.adam@azets.com>
2023-01-04 14:33:42 +0000

    git

---
+++ setup/windows/1.ps1
@@ -0,0 +1,4 @@
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+#RESTART REQUIRED
+shutdown /r /t 0
