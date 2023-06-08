1cca9d5eefe3c9d62db5593f8dc3919db0721572
Mircea Adam <mirciulicah@gmail.com>
2023-06-08 22:15:39 +0300

    win:meniu

--- v4/windows/setup.ps1
+++ v4/windows/setup.ps1
@@ -5,6 +5,7 @@ $INSTALL_DIR = 'C:\Temp\CDE'
 $REPO_HOME = 'C:\Temp\CDE\MSCODE-CDE\MSCODE-CDE-main'
 $AWS = "$HOME\.aws"
 $USER = $env:UserName
+$PacketManagersFolder = "$REPO_HOME\v4\windows\packet-manager"
 
 
 # ESENTIAL CHECKS
@@ -26,9 +27,8 @@ else {
 
 # PACKAGE MANAGERS CHECK
 Write-Host "Setup is checking local package managers.."
-$scriptFolder = "$REPO_HOME\v4\windows\packet-manager"  # Replace with the path to your script folder
 
-$scripts = Get-ChildItem -Path $scriptFolder -Filter "*.ps1" | Sort-Object
+$scripts = Get-ChildItem -Path $PacketManagersFolder -Filter "*.ps1" | Sort-Object
 
 foreach ($script in $scripts) {
     Write-Host "Running script: $($script.Name)"
@@ -39,15 +39,14 @@ foreach ($script in $scripts) {
 # Ask what to install: minimal or full or update
 function Show-Help {
     Write-Host "Usage: Choose a setup option:"
-    Write-Host "  - minimal: Displays minimal setup information."
-    Write-Host "  - full: Displays full setup information."
-    Write-Host "  - update: Displays update information for existing installation."
-    Write-Host ""
-    Write-Host "Example: .\setup.ps1 full"
+    Write-Host "  - minimal: awscli + mfa (mandatory), python+pip+code-commit-helper (optional)"
+    Write-Host "  - full: WSL full configuration + MSCODE + Docker(optional) + minimal setup (optional) "
+    Write-Host "  - update: //under construction: Will check and update Docker + MSCODE + python + git (and update)"
+    Write-Host "Actually TYPE minimal, full or update"
 }
 
 function Show-MinimalSetup {
-    Write-Host "Minimal Setup:"
+    Write-Host "[ minimal ]"
     Write-Host "- awscli"
     Write-Host "- awscli MFA: getToken (script) + add to Environment Variables"
     Write-Host "- git (optional)"
@@ -57,7 +56,7 @@ function Show-MinimalSetup {
 }
 
 function Show-FullSetup {
-    Write-Host "Full Setup:"
+    Write-Host "[ full ]"
     Write-Host "- Minimal Setup (optional)"
     Write-Host "- Windows Settings to enable WSL Properly"
     Write-Host "- Windows Kernel Update for WSL"
@@ -67,9 +66,23 @@ function Show-FullSetup {
     Write-Host "- Docker: latest (optional)"
 }
 
+function Show-UpdateSetup {
+    Write-Host "Update:"
+    Write-Host "//under construction: Will check and update Docker + MSCODE + python + git (and update)"
+}
+
+function Render-Meniu {
+    Write-Host "..::: Welcome to Windows Custom Development Environment version 4.0 ::.."
+    Write-Host "THE SETUP OPTIONS ARE:"
+    Show-MinimalSetup
+    Show-FullSetup
+    Show-UpdateSetup
+}
+
 $validOptions = @("minimal", "full", "update")
 
-$selectedOption = Read-Host "Choose a setup option (minimal, full, update):"
+Render-Meniu
+$selectedOption = Read-Host "Type Exactly the word for what setup type you would like: [ minimal ] or [ full ] or [ update ]"
 
 if (-not $validOptions.Contains($selectedOption.ToLower())) {
     Show-Help
@@ -83,7 +96,7 @@ else {
             Show-FullSetup
         }
         "update" {
-            Write-Host "Update information for existing installation."
+            Show-UpdateSetup
         }
     }
 }
