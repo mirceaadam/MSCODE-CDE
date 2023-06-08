855ddcc7920a48a2ea6875799340652cb7acf41b
Mircea Adam <mirciulicah@gmail.com>
2023-06-09 01:31:46 +0300

    win:minimal:testing7

--- v4/windows/managers/optional/python-pip.ps1
+++ v4/windows/managers/optional/python-pip.ps1
@@ -1,6 +1,6 @@
 $packageName = "Python.Python.3.11"
     # Install Python using WinGet
     Write-Host "Installing $packageName using WinGet..."
-    winget install $packageName -q
+    winget install -q $packageName
     Write-Host "Installing pip.. "
     python -m pip install --upgrade pip
