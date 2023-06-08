d0016a2bc9cf9b1f2c42beb8470baf8edbc26e8d
Mircea Adam <mirciulicah@gmail.com>
2023-06-08 18:22:18 +0300

    added latest version of awscli

--- v4/wsl/software/install.sh
+++ v4/wsl/software/install.sh
@@ -14,19 +14,27 @@ sudo apt-get install -y -qq \
     jq \
     wget
 
-pip3 install \
-    awscli \
-    git-remote-codecommit
+
+
+# AWSCLI Latest
+sudo apt install glibc-source groff less unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+# git-remote-codecommit
+pip3 install git-remote-codecommit
 
 # NVM / NODE
 # check this: https://learn.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-wsl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm use node
-# curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-# sh nodesource_setup.sh
-#install CDK
-npm install -g aws-cdk    
+
+# CDK
+npm install -g aws-cdk
+
 # Uses "robbyrussell" theme (original Oh My Zsh theme), with no plugins
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
     -t robbyrussell
