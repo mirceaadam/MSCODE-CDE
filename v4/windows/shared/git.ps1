0be14fca6c2cf6148ba4b2edac8892b0d7e41f66
Mircea Adam <mirciulicah@gmail.com>
2023-06-09 16:08:01 +0300

    win:minimal:Env::FinalTouches

--- v4/windows/shared/git.ps1
+++ v4/windows/shared/git.ps1
@@ -12,22 +12,28 @@ function InstallGit {
     }
 
 function AddGitToENV {
+    # Get the current system PATH environment variable
+    $currentPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine')
 
-# Get the current system PATH environment variable
-$currentPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine')
+    # Check if the GIT directory is already in the PATH
+    if ($currentPath -split ';' -contains $GitInstallPath) {
+        Write-Host "GIT is already in the system PATH."
+    }
+    else {
 
-# Check if the GIT directory is already in the PATH
-if ($currentPath -split ';' -contains $GitInstallPath) {
-    Write-Host "GIT is already in the system PATH."
+        # Update the system PATH environment variable
+        [Environment]::SetEnvironmentVariable("Path", "$env:Path;$GitInstallPath1", [EnvironmentVariableTarget]::User)
+        [Environment]::SetEnvironmentVariable("Path", "$env:Path;$GitInstallPath2", [EnvironmentVariableTarget]::User)
+        Write-Host "GIT has been added to the system PATH."
+    }
 }
-else {
 
-    # Update the system PATH environment variable
-    [Environment]::SetEnvironmentVariable("Path", "$env:Path;$GitInstallPath1", [EnvironmentVariableTarget]::User)
-    [Environment]::SetEnvironmentVariable("Path", "$env:Path;$GitInstallPath2", [EnvironmentVariableTarget]::User)
-    Write-Host "GIT has been added to the system PATH."
-}
-}
 
-InstallGit
-AddGitToENV
+Write-Host "Install git?.."
+$input = Read-Host "Enter [y]es or [n]o:"
+if ($input -eq "y") {
+    InstallGit
+    AddGitToENV
+} else {
+    Write-Host "Git Install skipped."
+}
