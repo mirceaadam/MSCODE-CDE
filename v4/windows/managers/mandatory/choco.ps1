57647e4f44b7a0c0e857549a706dec957e9e8cf6
Mircea Adam <mirciulicah@gmail.com>
2023-06-09 03:11:20 +0300

    win:minimal:testing13:FixChocoIssue3

--- v4/windows/managers/mandatory/choco.ps1
+++ v4/windows/managers/mandatory/choco.ps1
@@ -1,12 +1,12 @@
 $chocoInstalled = Get-Command choco.exe -ErrorAction SilentlyContinue
 
 if (-not $chocoInstalled) {
-    Chocolatey is not installed, so install it
+    #Chocolatey is not installed, so install it
     Set-ExecutionPolicy Bypass -Scope Process -Force
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 else {
-    Chocolatey is installed, check for updates
+    #Chocolatey is installed, check for updates
     $latestVersion = (choco upgrade chocolatey --noop --whatif) 2>&1 | Select-String -Pattern "upgrading from (\d+\.\d+\.\d+\.\d+)" -AllMatches | Foreach-Object { $_.Matches.Groups[1].Value }
 
     if (-not [string]::IsNullOrEmpty($latestVersion)) {
