496908f7a80df4fc75c9e83b4303575abb7afb44
Mircea Adam <mirciulicah@gmail.com>
2023-08-02 18:50:36 +0300

    Update install.sh
    disable zsh install

--- v4/wsl/software/install.sh
+++ v4/wsl/software/install.sh
@@ -50,5 +50,5 @@ sudo usermod -aG docker $USER
 npm install -g aws-cdk
 
 # Uses "robbyrussell" theme (original Oh My Zsh theme), with no plugins
-sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
-    -t robbyrussell
+#sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
+#    -t robbyrussell
