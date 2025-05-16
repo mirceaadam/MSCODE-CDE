6764a0c924e1a4bcf0b246b63968148ebf1650df
Mircea Adam <mirciulicah@gmail.com>
2025-05-16 08:32:27 +0000

    better generation

--- v4/windows/shared/gitlabAccess.ps1
+++ v4/windows/shared/gitlabAccess.ps1
@@ -3,14 +3,26 @@ $packageName = "openssh"
 
 function GenerateSSHKeyPair {
     
-        Write-Host "Starting the gen process.." 
-        Write-Host "Just hit ENTER for all questions."
-        ssh-keygen -t rsa -b 2048 -C "$USER GitlabAccess Key"
+        Write-Host "Starting the gen process.."
+        $input_email = Read-Host "Enter your email address: "
+        Write-Host "Using: $input_email"
+        Write-Host " "
+        Write-Host " " 
+        Write-Host "Hint: Just hit << ENTER >> for all questions."
+        Write-Host " "
+        Write-Host " "
+        ssh-keygen -t rsa -b 4096 -C "$input_email"
     }
 
 function DisplayHelp {  
-    Write-Host "Visit https://gitlab.company.xyz/-/user_settings/ssh_keys and add the from notepad."
+    Write-Host " "
+    Write-Host " "
+    Write-Host " Generation Completed. "
+    Write-Host "NEXT - Visit: https://gitlab.company.xyz/-/user_settings/ssh_keys"
+    Write-Host " "
+    Write-Host " Add the full contents from the opened notepad to the << KEY >> section in GitLab SSH Keys under your $USER user profile. "    
     notepad "C:\Users\$USER\.ssh\id_rsa.pub"
+    Write-Host " "
     Write-Host "(!) If above failed, visit: https://docs.gitlab.com/user/ssh/#generate-an-ssh-key-pair"
 }
 
